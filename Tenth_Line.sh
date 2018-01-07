f16e1e8e2acf74ad9c57eca4e448226e4b3c03f6
hesunnb <hesun63@gmail.com>
2018-01-06 19:43:03 -0500

    Update Tenth_Line.sh

--- Tenth_Line.sh
+++ Tenth_Line.sh
@@ -16,10 +16,11 @@ echo ""
 # Solution 2
 awk 'FNR == 10 {print }'  file.txt
 # OR
-awk 'NR == 10' file.txt
+awk 'NR == 10' file.txt //NR,表示awk开始执行程序后所读取的数据行数.FNR,与NR功用类似,不同的是awk每打开一个新文件,FNR便从0重新累计.
+			//这里是只读取第十行
 
 # Solution 3
-sed -n 10p file.txt
+sed -n 10p file.txt //-n是静默模式, p是只显示符合条件的行
 
 # Solution 4
-tail -n+10 file.txt|head -1
+tail -n+10 file.txt|head -1 //-n+10, 表示从头删掉10-1行之后开始
